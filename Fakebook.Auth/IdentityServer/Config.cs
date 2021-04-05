3ec6b430bbdf4eb81a35ce6c303d5527c98b32fd
Caleb Owens <owenscaleb75@gmail.com>
2021-04-05 17:04:20 -0500

    adjust IdentityServer config, URI paths

--- Fakebook.Auth/IdentityServer/Config.cs
+++ Fakebook.Auth/IdentityServer/Config.cs
@@ -10,11 +10,11 @@ namespace IdentityServer
     public static class Config
     {
         public static IEnumerable<IdentityResource> IdentityResources =>
-                   new IdentityResource[]
-                   {
+            new List <IdentityResource>
+            {
                 new IdentityResources.OpenId(),
                 new IdentityResources.Profile(),
-                   };
+            };
 
         public static IEnumerable<ApiScope> ApiScopes =>
             new ApiScope[]
@@ -29,11 +29,12 @@ namespace IdentityServer
                 // m2m client credentials flow client
                 new Client
                 {
-                    ClientId = "m2m.client",
+                    ClientId = "client",
                     ClientName = "Client Credentials Client",
 
                     AllowedGrantTypes = GrantTypes.ClientCredentials,
-                    ClientSecrets = { new Secret("511536EF-F270-4058-80CA-1C89C192F69A".Sha256()) },
+                    // old secret 511536EF-F270-4058-80CA-1C89C192F69A
+                    ClientSecrets = { new Secret("secret".Sha256()) },
 
                     AllowedScopes = { "scope1" }
                 },
@@ -41,14 +42,15 @@ namespace IdentityServer
                 // interactive client using code flow + pkce
                 new Client
                 {
-                    ClientId = "interactive",
-                    ClientSecrets = { new Secret("49C1A7E1-0C79-4A89-A3D6-A37998FB86B0".Sha256()) },
+                    ClientId = "mvc",
+                    // old secret "49C1A7E1-0C79-4A89-A3D6-A37998FB86B0
+                    ClientSecrets = { new Secret("secret".Sha256()) },
 
                     AllowedGrantTypes = GrantTypes.Code,
 
-                    RedirectUris = { "https://localhost:44300/signin-oidc" },
-                    FrontChannelLogoutUri = "https://localhost:44300/signout-oidc",
-                    PostLogoutRedirectUris = { "https://localhost:44300/signout-callback-oidc" },
+                    RedirectUris = { "https://localhost:5001/signin-oidc" },
+                    FrontChannelLogoutUri = "https://localhost:5001/signout-oidc",
+                    PostLogoutRedirectUris = { "https://localhost:5001/signout-callback-oidc" },
 
                     AllowOfflineAccess = true,
                     AllowedScopes = { "openid", "profile", "scope2" }
